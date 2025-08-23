8c0ac442630b1ad38ac682cdc8286c815d3e681e
carlosresendeP <carlosresende.paula@gmail.com>
2025-08-23 08:52:00 -0300

    corrigir links dos projetos para abrir corretamente

--- js/cardProjects-MyprojetctsPage.js
+++ js/cardProjects-MyprojetctsPage.js
@@ -27,7 +27,7 @@ function showProjects(projects) {
 
                         <div class="project-links">
                             
-                            <a href=../../${project.linkSite} >Acessar Pagina</a>
+                            <a href="${project.linkSite}" >Acessar Pagina</a>
                         </div>    
                     </div>
                 </div>
@@ -45,8 +45,8 @@ function showProjects(projects) {
                         </div>    
 
                         <div class="project-links">
-                            ${project.linkFont ? `<a href="../../${project.linkFont}" target="_blank">Código</a>` : ''}
-                            ${project.linkSite ? `<a href="../../${project.linkSite}" target="_blank">Live</a>` : ''}
+                            ${project.linkFont ? `<a href="${project.linkFont}" target="_blank">Código</a>` : ''}
+                            ${project.linkSite ? `<a href="${project.linkSite}" target="_blank">Live</a>` : ''}
 
                         </div>    
                     </div>
